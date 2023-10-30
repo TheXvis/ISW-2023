54a3681fc70004a4619f5ba76fae8f7e5a7533a9
MatiasVillasenor <matias.villasenor1701@alumnos.ubiobio.cl>
2023-10-30 04:49:45 -0300

    añadido creación y manejo de fichas sociales

---
+++ routes/fichaRoutes.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const FichaSocialModel = require('../models/fichaSocial');
+const UserModel = require('../models/user');
+const authMiddleware = require('../auth');
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+const app = express();
+
+// Ruta para crear una ficha social
+app.post('/crearficha', authMiddleware, async (req, res) => {
+  try {
+    // Asegúrate de que solo los asistentes sociales puedan crear fichas sociales
+    if (req.user.role !== 'asistente') {
+      return res.status(403).json({ message: 'No tienes permiso para realizar esta acción' });
+    }
+
+    // Obtén los datos de la ficha social del cuerpo de la solicitud
+    const fichaSocialData = req.body;
+
+    // Asocia la ficha social al asistente social que la está creando
+    fichaSocialData.usuarioId = req.user.userId;
+
+    // Crea una nueva ficha social utilizando el modelo y guarda en la base de datos
+    const fichaSocial = new FichaSocialModel(fichaSocialData);
+    await fichaSocial.save();
+
+    res.status(201).json({ message: 'Ficha social creada exitosamente' });
+  } catch (error) {
+    res.status(400).json({ message: 'No se pudo crear la ficha social', error: error.message });
+  }
+});
+
+// Ruta para obtener una ficha social por su ID
+app.get('/:id', authMiddleware, async (req, res) => {
+  try {
+    // Implementa la lógica para obtener una ficha social por su ID
+    // Asegúrate de que solo los asistentes sociales puedan acceder a esta ruta
+
+    // Ejemplo:
+    const fichaSocial = await FichaSocialModel.findById(req.params.id);
+
+    if (!fichaSocial) {
+      return res.status(404).json({ message: 'Ficha social no encontrada' });
+    }
+
+    res.json(fichaSocial);
+  } catch (error) {
+    res.status(500).json({ message: 'Error al obtener la ficha social', error: error.message });
+  }
+});
+
+// Ruta para actualizar una ficha social por su ID
+app.put('/actualizarficha/:id', authMiddleware, async (req, res) => {
+    try {
+      // Asegúrate de que solo los asistentes sociales puedan actualizar fichas sociales
+      if (req.user.role !== 'asistente') {
+        return res.status(403).json({ message: 'No tienes permiso para realizar esta acción' });
+      }
+  
+      const fichaSocialId = req.params.id;
+      const fichaSocialData = req.body;
+  
+      // Implementa la lógica para actualizar la ficha social utilizando el modelo y el ID
+      const fichaSocialActualizada = await FichaSocialModel.findByIdAndUpdate(fichaSocialId, fichaSocialData, { new: true });
+  
+      if (!fichaSocialActualizada) {
+        return res.status(404).json({ message: 'Ficha social no encontrada' });
+      }
+  
+      res.json({ message: 'Ficha social actualizada exitosamente', fichaSocial: fichaSocialActualizada });
+    } catch (error) {
+      res.status(500).json({ message: 'Error al actualizar la ficha social', error: error.message });
+    }
+  });
+  
+  // Ruta para eliminar una ficha social por su ID
+  app.delete('/eliminarficha/:id', authMiddleware, async (req, res) => {
+    try {
+      // Asegúrate de que solo los asistentes sociales puedan eliminar fichas sociales
+      if (req.user.role !== 'asistente') {
+        return res.status(403).json({ message: 'No tienes permiso para realizar esta acción' });
+      }
+  
+      const fichaSocialId = req.params.id;
+  
+      // Implementa la lógica para eliminar la ficha social utilizando el modelo y el ID
+      const fichaSocialEliminada = await FichaSocialModel.findByIdAndDelete(fichaSocialId);
+  
+      if (!fichaSocialEliminada) {
+        return res.status(404).json({ message: 'Ficha social no encontrada' });
+      }
+  
+      res.json({ message: 'Ficha social eliminada exitosamente' });
+    } catch (error) {
+      res.status(500).json({ message: 'Error al eliminar la ficha social', error: error.message });
+    }
+  });
+  
+
+module.exports = app;
