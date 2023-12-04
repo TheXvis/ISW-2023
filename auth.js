9433dc50d8c8a97ee90db3c6434a9c4a6883b4c4
TheXvis <charly.brantt1901@alumnos.ubiobio.cl>
2023-12-04 11:45:49 -0300

    fixBearerAuth

--- auth.js
+++ auth.js
@@ -2,11 +2,14 @@ const jwt = require('jsonwebtoken');
 const config = require('./config');
 
 function auth(req, res, next){
-  const token = req.header('Authorization');
+  const bearerToken = req.header('Authorization');
 
-  if(!token){
+  if(!bearerToken){
     return res.status(401).json({message: 'Acceso denegado'});
   }
+
+  const token = bearerToken.split(' ')[1];
+
   try{
     const decoded = jwt.verify(token, config.secretKey);
     req.user = decoded;
